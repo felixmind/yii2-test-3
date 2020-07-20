5b7eb2076b5022ffcb46989d380d6c2841d941e9
felixmind <felixmind@outlook.com>
2020-07-20 23:47:20 +0300

    Фикстуры

---
+++ src/tests/unit/fixtures/FeedbackFixture.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace tests\unit\fixtures;
+
+use yii\test\ActiveFixture;
+use app\models\Feedback;
+
+class FeedbackFixture extends ActiveFixture
+{
+    public $modelClass = Feedback::class;
+}
---
+++ src/tests/unit/fixtures/data/feedbacks.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    [
+        'customer' => 'divan',
+        'phone'    => '+7(999)999-99-99',
+    ],
+    [
+        'customer' => 'anonymous',
+        'phone'    => '+0(000)000-00-00',
+    ],
+];
